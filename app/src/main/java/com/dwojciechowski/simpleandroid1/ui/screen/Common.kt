faa8b5f87ca47d00dafa75d172c7cbfb87f8f6fa
Dominik Wojciechowski <doominikwoojciechowski@gmail.com>
2022-02-25 21:52:44 +0100

    modified common components to not stand out so much.

--- app/src/main/java/com/dwojciechowski/simpleandroid1/ui/screen/Common.kt
+++ app/src/main/java/com/dwojciechowski/simpleandroid1/ui/screen/Common.kt
@@ -2,7 +2,6 @@ package com.dwojciechowski.simpleandroid1.ui.screen
 
 import android.util.Pair
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,16 +14,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.dwojciechowski.simpleandroid1.ui.nav.Screen
+import com.dwojciechowski.simpleandroid1.ui.theme.MainAppTheme
 
 @Composable
 fun ItemView(items: List<Pair<String, String>>, navController: NavController) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp)
+            .padding(start = 10.dp, end = 10.dp)
     ) {
         items(items) { item ->
             DisplayRow(item.first, item.second) {
@@ -35,42 +37,53 @@ fun ItemView(items: List<Pair<String, String>>, navController: NavController) {
     }
 }
 
+@Preview
+@Composable
+fun ItemViewPreview() {
+
+    val listOf = listOf(
+        Pair("AA", "AAAAAAAAAAAAAAAAAAA"),
+        Pair("AA", "AAAAAAAAAAAAAAAAAAA"),
+        Pair("AA", "AAAAAAAAAAAAAAAAAAA"),
+        Pair("AA", "AAAAAAAAAAAAAAAAAAA"),
+        Pair("AA", "AAAAAAAAAAAAAAAAAAA"),
+    )
+    val rememberNavController = rememberNavController()
+    MainAppTheme(darkTheme = true) {
+        Surface(color = MaterialTheme.colors.background) {
+            ItemView(listOf, rememberNavController)
+        }
+    }
+}
+
+
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DisplayRow(logo: String = "NN", description: String = "Lorem Ipsum", onClick: () -> Unit) {
 
     Surface(
         shape = RoundedCornerShape(25),
-        elevation = 5.dp,
-        onClick = onClick
+        elevation = 10.dp,
+        color = MaterialTheme.colors.background,
+        onClick = onClick,
+        modifier = Modifier.padding(top = 5.dp)
     ) {
         Row(
             horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth(1.0f)
-                .background(MaterialTheme.colors.background)
-                .border(
-                    width = 2.dp,
-                    color = MaterialTheme.colors.secondary,
-                    shape = RoundedCornerShape(25)
-                )
-                .padding(10.dp)
+                .padding(7.dp)
         ) {
 
             Text(
                 textAlign = TextAlign.Center,
                 text = logo,
                 style = MaterialTheme.typography.h6,
-                color = MaterialTheme.colors.onSecondary,
+                color = MaterialTheme.colors.background,
                 modifier = Modifier
-                    .background(color = MaterialTheme.colors.primary, CircleShape)
+                    .background(color = MaterialTheme.colors.onBackground, CircleShape)
                     .size(40.dp)
-                    .border(
-                        width = 2.dp,
-                        color = MaterialTheme.colors.primaryVariant,
-                        shape = CircleShape
-                    )
                     .wrapContentHeight(align = Alignment.CenterVertically)
             )
 
@@ -79,7 +92,16 @@ fun DisplayRow(logo: String = "NN", description: String = "Lorem Ipsum", onClick
             Text(description)
         }
     }
+}
 
+@Preview
+@Composable
+fun DisplayRowPreview() {
+    MainAppTheme {
+        DisplayRow {
+
+        }
+    }
 }
 
 @Composable
